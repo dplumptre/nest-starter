9e3e0cb773898601abeb57f2c47cab23c28d799e
Ademola <dplumptre@yahoo.com>
2026-03-07 23:09:37 +0000

    updated hello

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
 
+interface User {
+  name: string;
+  age: number;
+}
+
 @Injectable()
 export class AppService {
+
+
+
   getHello(): string {
-    return 'Hello World!';
+
+    return 'Hello World!' + this.userData({ name: 'John', age: 'thirty' });
   }
+
+
+ private  userData=( data: User): string => {
+    return `User ${data.name} is ${data.age} years old.`;
+ }
+
+
 }
