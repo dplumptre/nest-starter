4643bfbd46506b8afa16e15b4863e9f067e35b48
Ademola <dplumptre@yahoo.com>
2026-03-08 00:41:34 +0000

    update

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
 
+
+
 @Injectable()
 export class AppService {
+
+  private appName: string = 'Nest';
   getHello(): string {
-    return 'Hello World now!';
+    return `${this.appName} Hello World now!`;
   }
+
+  getAppName(): string {
+    return 44;
+  }
+
 }
